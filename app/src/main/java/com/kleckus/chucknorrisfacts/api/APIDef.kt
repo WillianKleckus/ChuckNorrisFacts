6e74cfb04f52b6eb3ec4298df2bb25aa5a803c00
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2020-09-01 21:37:34 -0300

    Fixed the bug that was crashing the app when querying for a joke.

--- app/src/main/java/com/kleckus/chucknorrisfacts/api/APIDef.kt
+++ app/src/main/java/com/kleckus/chucknorrisfacts/api/APIDef.kt
@@ -2,23 +2,23 @@ package com.kleckus.chucknorrisfacts.api
 
 import io.reactivex.Observable
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 private const val RANDOM_ENDPOINT = "random"
 private const val CATEGORIES_ENDPOINT = "categories"
-private const val RANDOM_FROM_CATEGORY_ENDPOINT ="random?category={category}"
-private const val QUERY_ENDPOINT = "search?query={query}"
+private const val RANDOM_FROM_CATEGORY_ENDPOINT ="random"
+private const val QUERY_ENDPOINT = "search"
 
 interface APIDef {
     @GET(RANDOM_ENDPOINT)
     fun getRandomJoke() : Observable<JokeResult>
 
     @GET(RANDOM_FROM_CATEGORY_ENDPOINT)
-    fun getRandomJokeFrom(@Path("category") category : String) : Observable<JokeResult>
+    fun getRandomJokeFrom(@Query("category") category : String) : Observable<JokeResult>
 
     @GET(CATEGORIES_ENDPOINT)
     fun getAvailableCategories() : Observable<MutableList<String>>
 
     @GET(QUERY_ENDPOINT)
-    fun queryForJoke(@Path("query") query : String) : Observable<QueryResult>
+    fun queryForJoke(@Query("query") query : String) : Observable<QueryResult>
 }
