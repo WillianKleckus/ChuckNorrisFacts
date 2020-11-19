8b8f60769d7994de24999ba9b6580c5be7c1cbe8
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2020-11-19 10:19:12 -0300

    feat: fully implemented the UI

--- domain/src/main/java/com/kleckus/domain/models/Constants.kt
+++ domain/src/main/java/com/kleckus/domain/models/Constants.kt
@@ -2,4 +2,5 @@ package com.kleckus.domain.models
 
 object Constants {
     const val QUERY_MINIMUM_LENGTH = 4
+    const val NO_CATEGORY = "UNCATEGORIZED"
 }
--- ui/src/main/java/com/kleckus/ui/activities/MainActivity.kt
+++ ui/src/main/java/com/kleckus/ui/activities/MainActivity.kt
@@ -3,30 +3,46 @@ package com.kleckus.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.textfield.TextInputEditText
 import com.kleckus.domain.models.Joke
 import com.kleckus.ui.R
-import com.kleckus.ui.toastMsg
+import com.kleckus.ui.adapters.JokeAdapter
 import com.kleckus.ui.vm.ApiViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel = ApiViewModel(this)
+    private val viewModel by lazy{
+        val vm = ApiViewModel(this)
+        vm.onResult = ::onResult
+        vm
+    }
+    private val adapter by lazy { JokeAdapter(this,::onSharing) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.onResult = ::onResult
 
+        setTextListener()
+        startRecyclerView()
+    }
+
+    private fun setTextListener(){
         searchView.setStartIconOnClickListener {
             val text = searchView.editText?.text.toString()
             viewModel.searchFor(text)
         }
     }
 
-    private fun onResult(list : MutableList<Joke>){
-        Log.i("CN Log", list.toString())
+    private fun startRecyclerView(){
+        recyclerView.adapter = adapter
+    }
+
+    private fun onResult(jokeList : MutableList<Joke>){
+        Log.i("CN Log", jokeList.toString())
+        adapter.changeDataSet(jokeList)
+    }
+
+    private fun onSharing(joke : Joke){
+        Log.i("CN Log", "Sharing joke -> ${joke.value}")
     }
 }
